3ea6908eb579b34c1132c7947c6a3ac6f1300dba
wei <bear920430@gmail.com>
2024-01-23 09:40:11 +0800

    Updates

---
+++ precache-manifest.b290a760883c806594ca3e6f3f1e61ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c93febf10586c3c13f0fedcdc81bad8",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "e64244ff6bcfe8a66809",
+    "url": "/todolist/static/css/main.f4b44880.chunk.css"
+  },
+  {
+    "revision": "2588395ffbb7d912489c",
+    "url": "/todolist/static/js/2.5fb19982.chunk.js"
+  },
+  {
+    "revision": "e64244ff6bcfe8a66809",
+    "url": "/todolist/static/js/main.3a84390b.chunk.js"
+  },
+  {
+    "revision": "5225356577f332e6df46",
+    "url": "/todolist/static/js/runtime~main.d3bf418b.js"
+  }
+]);
